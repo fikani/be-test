e9c52541b43d4ac0727da9da2770a48f51bf17cf
Afif Fikani <afif.fikani@gmail.com>
2022-02-24 01:53:17 -0300

    feat(balance): add transaction + lock

--- src/controllers/balances.js
+++ src/controllers/balances.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { Profile } = require("../model");
+const { Transaction } = require("sequelize");
 const router = Router();
 
 router.post("/balances/deposit/:userId", async (req, res) => {
@@ -13,30 +14,37 @@ router.post("/balances/deposit/:userId", async (req, res) => {
   }
 
   const sequelize = req.app.get("sequelize");
-  const jobs = await Job.findOne({
-    attributes: [[sequelize.fn("sum", sequelize.col("price")), "total"]],
-    where: { paid: false },
-    include: [
-      {
-        model: Contract,
-        where: {
-          ClientId: userId,
-          status: "in_progress",
-        },
-      },
-    ],
-  });
+  await sequelize.transaction(
+    { isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE },
+    async (t) => {
+      const jobs = await Job.findOne({
+        attributes: [[sequelize.fn("sum", sequelize.col("price")), "total"]],
+        where: { paid: false },
+        include: [
+          {
+            model: Contract,
+            where: {
+              ClientId: userId,
+              status: "in_progress",
+            },
+          },
+        ],
+        lock: t.LOCK.UPDATE,
+        transaction: t,
+      });
 
-  const total = (jobs && jobs.get("total")) || 0;
-  if (value <= 0.25 * total) {
-    Profile.update(
-      { balance: sequelize.literal("balance + " + value) },
-      { where: { id: userId } }
-    );
-    res.send();
-  } else {
-    res.status(412).json({ msg: "Value is to high." });
-  }
+      const total = (jobs && jobs.get("total")) || 0;
+      if (value <= 0.25 * total) {
+        await Profile.update(
+          { balance: sequelize.literal("balance + " + value) },
+          { where: { id: userId }, lock: t.LOCK.UPDATE, transaction: t }
+        );
+        res.send();
+      } else {
+        res.status(412).json({ msg: "Value is to high." });
+      }
+    }
+  );
 });
 
 module.exports = {
